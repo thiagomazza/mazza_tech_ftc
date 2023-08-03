c8385cff68be7eb5a24c046263cf1af78a7020af
Thiago Mazza <112523811+thiagomazza@users.noreply.github.com>
2023-08-03 11:23:50 -0300

    Update 🌎Countries.py

--- pages/🌎Countries.py
+++ pages/🌎Countries.py
@@ -58,7 +58,7 @@ st.sidebar.markdown("## Filtros")
 countries = st.sidebar.multiselect(
     "Escolha os Paises que deseja filtrar:",
     df_aux.loc[:, "Country Code"].unique().tolist(),
-    default=["Brazil", "England", "Qatar", "South Africa", "Canada", "Australia"],
+    default=df_aux.loc[:, "Country Code"].unique().tolist(),
 )
 
 # Filtro do DataFrame com base nos países selecionados
